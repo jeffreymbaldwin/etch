6baddc78cf7e14f523519596fd0557420794c479
Jeff <jmartinbaldwin@gmail.com>
2025-02-06 08:43:13 +0900

    Edit the css to make a 16 x 16 gride using 50px squares.

--- etch.js
+++ etch.js
@@ -1,7 +1,7 @@
 for(let i =0; i<16; i++){
     for(let j = 0; j < 16; j++) {
        const newDiv = document.createElement("div");
-       newDiv.textContent = "Hello";
+       newDiv.classList.add("grid-item");
         document.querySelector("#container").appendChild(newDiv);
     
     }
