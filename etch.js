5326924fc160093ee4336a3bdf9d291f9ae237df
Jeff <jmartinbaldwin@gmail.com>
2025-02-05 14:44:05 +0900

    add the 16 x 16 grid and hello so I can see each of the squares

--- etch.js
+++ etch.js
@@ -1,5 +1,8 @@
 for(let i =0; i<16; i++){
     for(let j = 0; j < 16; j++) {
-        document.createElement("div");
+       const newDiv = document.createElement("div");
+       newDiv.textContent = "Hello";
+        document.querySelector("#container").appendChild(newDiv);
+    
     }
 }
