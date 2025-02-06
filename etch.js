84b4cd2922daca70a36ce056ed32eff1648304f0
Jeff <jmartinbaldwin@gmail.com>
2025-02-06 15:50:10 +0900

    Center the grid in the middle of the webpage
    Add a button and the prompt function to the
    button
    
    Add mouseover function to highlight the and color
    the squares blue when the mouser goes over them

--- etch.js
+++ etch.js
@@ -1,8 +1,22 @@
-for(let i =0; i<16; i++){
-    for(let j = 0; j < 16; j++) {
+for(let i =0; i<33; i++){
+    for(let j = 0; j < 33; j++) {
        const newDiv = document.createElement("div");
        newDiv.classList.add("grid-item");
         document.querySelector("#container").appendChild(newDiv);
+
+        newDiv.addEventListener("mouseover", ( ) => {
+            newDiv.style.backgroundColor = "blue";
+        })
     
     }
+}
+
+document.querySelector("#button").addEventListener("click",() => {
+    getUserInput();
+    //changeForLoop();
+    //changeHeightAndWidth();
+})
+
+function getUserInput (){
+    prompt("Enter a number for your square x square. No number larger than 100 please!");
 }
