d55cdffeccf2f63cf70b5df954d7fb61b7c84a7e
Jeff <jmartinbaldwin@gmail.com>
2025-02-07 14:02:34 +0900

    Put the for loop in a function. Fixes the previous mistake of the grid disappearing.

--- etch.js
+++ etch.js
@@ -1,13 +1,16 @@
-for(let i =0; i<64; i++){
-    for(let j = 0; j <64; j++) {
-       const newDiv = document.createElement("div");
-       newDiv.classList.add("grid-item");
-        document.querySelector("#container").appendChild(newDiv);
-
-        newDiv.addEventListener("mouseover", ( ) => {
-            newDiv.style.backgroundColor = "blue";
-        })
+function createGrid16() {
+    for(let i =0; i<16; i++){
+        for(let j = 0; j <16; j++) {
+           const newDiv = document.createElement("div");
+           newDiv.classList.add("grid-item");
+            document.querySelector("#container").appendChild(newDiv);
     
+            newDiv.addEventListener("mouseover", ( ) => {
+                newDiv.style.backgroundColor = "blue";
+            })
+        
+        }
     }
 }
 
+createGrid16();
